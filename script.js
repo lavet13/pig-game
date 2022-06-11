173327504706e09169f6b261dfd0391d01ffdeb5
lavet13 <lavet13@mail.ru>
2022-06-11 09:13:17 +0300

    starting...

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+let diceRoll = Math.trunc(Math.random() * 6) + 1;
+let imageDiceRoll = document.createElement('img');
+
+const chooseImage = function (dice) {
+  switch (dice) {
+    case 1:
+      imageDiceRoll.src = 'dice-1.png';
+      break;
+    case 2:
+      imageDiceRoll.src = 'dice-2.png';
+      break;
+    case 3:
+      imageDiceRoll.src = 'dice-3.png';
+      break;
+    case 4:
+      imageDiceRoll.src = 'dice-4.png';
+      break;
+    case 5:
+      imageDiceRoll.src = 'dice-5.png';
+      break;
+    case 6:
+      imageDiceRoll.src = 'dice-6.png';
+      break;
+  }
+};
+
+chooseImage(diceRoll);
