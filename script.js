2c3eaf1051ba7e392654aac62b80a3947c74771e
lavet13 <lavet13@mail.ru>
2022-06-11 14:10:49 +0300

    not finished yet

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
 'use strict';
 
 let diceRoll = Math.trunc(Math.random() * 6) + 1;
-let imageDiceRoll = document.createElement('img');
+let prevImageDiceRoll = document.querySelector('.dice');
+let currentPlayer = 0;
 
-const chooseImage = function (dice) {
-  switch (dice) {
+const chooseImage = function () {
+  switch (diceRoll) {
     case 1:
-      imageDiceRoll.src = 'dice-1.png';
+      prevImageDiceRoll.src = 'dice-1.png';
       break;
     case 2:
-      imageDiceRoll.src = 'dice-2.png';
+      prevImageDiceRoll.src = 'dice-2.png';
       break;
     case 3:
-      imageDiceRoll.src = 'dice-3.png';
+      prevImageDiceRoll.src = 'dice-3.png';
       break;
     case 4:
-      imageDiceRoll.src = 'dice-4.png';
+      prevImageDiceRoll.src = 'dice-4.png';
       break;
     case 5:
-      imageDiceRoll.src = 'dice-5.png';
+      prevImageDiceRoll.src = 'dice-5.png';
       break;
     case 6:
-      imageDiceRoll.src = 'dice-6.png';
+      prevImageDiceRoll.src = 'dice-6.png';
       break;
   }
 };
 
-chooseImage(diceRoll);
+const addToCurrentScore = function () {
+  let currentScore = getCurrentScoreElement();
+  currentScore.textContent = Number(currentScore.textContent) + diceRoll;
+};
+
+const clearCurrentScore = function () {
+  getCurrentScoreElement().textContent = 0;
+};
+
+const addToPlayerScore = function () {
+  let currentScore = getCurrentScoreElement(),
+    playerScore = getPlayerScoreElement();
+  playerScore.textContent =
+    Number(playerScore.textContent) + Number(currentScore.textContent);
+  currentScore.textContent = 0;
+};
+
+const getPlayerScoreElement = function () {
+  return document.querySelector(`#score--${currentPlayer}`);
+};
+
+const getCurrentScoreElement = function () {
+  return document.querySelector(`#current--${currentPlayer}`);
+};
+
+const switchPlayer = function (currentPlayer) {
+  return currentPlayer === 0 ? 1 : 0;
+};
+
+const setNewDiceRoll = function () {
+  diceRoll = Math.trunc(Math.random() * 6) + 1;
+};
+
+const isOne = function (diceRoll) {
+  return diceRoll !== 1 ? true : false;
+};
+
+document.querySelector('.btn--roll').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (isOne(diceRoll)) {
+    chooseImage();
+    addToCurrentScore();
+  } else {
+    clearCurrentScore();
+    currentPlayer = switchPlayer(currentPlayer);
+    chooseImage();
+  }
+
+  setNewDiceRoll();
+});
+
+document.querySelector('.btn--hold').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  addToPlayerScore();
+  currentPlayer = switchPlayer(currentPlayer);
+});
